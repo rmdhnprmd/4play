3d97b264b4f493843c93686673400382c79f8f1d
rmdhnprmd <ramadhan.permadi36@gmail.com>
2021-10-20 17:42:53 +0700

    convert 404 page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import "../styles/sign-in.css";
 import "../styles/sign-up.css";
 import "../styles/sign-up-photo.css";
 import "../styles/sign-up-success.css";
+import "../styles/404-not-found.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
