ba441049facd67a6a2a1f9b23109972247b2ab22
rmdhnprmd <ramadhan.permadi36@gmail.com>
2021-10-20 17:34:21 +0700

    convert sign up page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
 import "../styles/sign-up.css";
 import "../styles/sign-up-photo.css";
+import "../styles/sign-up-success.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
