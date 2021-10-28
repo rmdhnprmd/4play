300e3c9c94e8872959711c9dbfb511deab6fa2bb
rmdhnprmd <ramadhan.permadi36@gmail.com>
2021-10-28 22:57:19 +0700

    refactor edit profile component

--- components/organisms/Sidebar/index.tsx
+++ components/organisms/Sidebar/index.tsx
@@ -2,19 +2,38 @@ import FooterSidebar from "./FooterSidebar";
 import Profile from "./Profile";
 import MenuItem from "./MenuItem";
 
-export default function Sidebar() {
+interface SidebarProps {
+  activeMenu: "overview" | "transactions" | "settings";
+}
+export default function Sidebar(props: SidebarProps) {
+  const { activeMenu } = props;
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem active title="Overview" icon="ic-menu-overview" />
-          <MenuItem title="Transactions" icon="ic-menu-transaction" />
-          <MenuItem title="Messages" icon="ic-menu-message" />
-          <MenuItem title="Card" icon="ic-menu-card" />
-          <MenuItem title="Rewards" icon="ic-menu-reward" />
-          <MenuItem title="Settings" icon="ic-menu-setting" />
-          <MenuItem title="Log Out" icon="ic-menu-logout" />
+          <MenuItem
+            active={activeMenu === "overview"}
+            title="Overview"
+            icon="ic-menu-overview"
+            href="/member"
+          />
+          <MenuItem
+            active={activeMenu === "transactions"}
+            title="Transactions"
+            icon="ic-menu-transaction"
+            href="/member/transactions"
+          />
+          <MenuItem title="Messages" icon="ic-menu-message" href="/member" />
+          <MenuItem title="Card" icon="ic-menu-card" href="/member" />
+          <MenuItem title="Rewards" icon="ic-menu-reward" href="/member" />
+          <MenuItem
+            active={activeMenu === "settings"}
+            title="Settings"
+            icon="ic-menu-setting"
+            href="/member/edit-profile"
+          />
+          <MenuItem title="Log Out" icon="ic-menu-logout" href="/sign-in" />
         </div>
         <FooterSidebar />
       </div>
